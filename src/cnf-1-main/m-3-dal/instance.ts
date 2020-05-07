599c3dcf15e486feb4d76f511ec58ef4361fd0ad
ai73aaa <1qazxcvBG>
2020-05-07 18:32:58 +0300

    operability checked

--- src/cnf-1-main/m-3-dal/instance.ts
+++ src/cnf-1-main/m-3-dal/instance.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
+import {DEV_VERSION} from "../../config";
 
-export const baseURL = 'https://cards-nya-back.herokuapp.com/';
+export const baseURL = DEV_VERSION
+    ? 'http://localhost:7542/'
+    : 'https://cards-nya-back.herokuapp.com/';
 
 export const instance = axios.create({
     baseURL
