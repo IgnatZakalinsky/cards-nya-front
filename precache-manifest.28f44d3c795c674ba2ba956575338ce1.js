8071bce96d2e6ef6b08a2a623e319c98a0d71d9c
ai73aaa <1qazxcvBG>
2020-05-07 18:40:18 +0300

    Updates

---
+++ precache-manifest.28f44d3c795c674ba2ba956575338ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8cf8d0945e6476328cc6ff8c06a27f",
+    "url": "/cards-nya-front/index.html"
+  },
+  {
+    "revision": "b05c2cb9cb240fb268ed",
+    "url": "/cards-nya-front/static/css/main.9a3bc64c.chunk.css"
+  },
+  {
+    "revision": "233ae24cbb26e5006c3d",
+    "url": "/cards-nya-front/static/js/2.51143480.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards-nya-front/static/js/2.51143480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05c2cb9cb240fb268ed",
+    "url": "/cards-nya-front/static/js/main.056bb736.chunk.js"
+  },
+  {
+    "revision": "bacd4495509446e6954f",
+    "url": "/cards-nya-front/static/js/runtime-main.564ac26c.js"
+  }
+]);
