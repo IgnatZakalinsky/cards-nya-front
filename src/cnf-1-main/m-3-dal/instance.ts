1e80524387b5f8c23c53a349a9c62730f954424d
ai73aaa <1qazxcvBG>
2020-05-08 17:40:04 +0300

    add backVersion

--- src/cnf-1-main/m-3-dal/instance.ts
+++ src/cnf-1-main/m-3-dal/instance.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import {DEV_VERSION} from "../../config";
 
 export const baseURL = DEV_VERSION
-    ? 'http://localhost:7542/'
-    : 'https://cards-nya-back.herokuapp.com/';
+    ? 'http://localhost:7542/1.0/'
+    : 'https://cards-nya-back.herokuapp.com/1.0/';
 
 export const instance = axios.create({
     baseURL
